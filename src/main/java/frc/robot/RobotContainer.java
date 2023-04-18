6403778fa796d65af272680e6360ca1013ff527b
rmm <112425985+RomanMartin100@users.noreply.github.com>
2023-04-18 22:23:00 +0000

    First version of drive train code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,10 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.DriveCommand;
+import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -19,17 +20,20 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  private final Drivetrain m_Drivetrain = new Drivetrain();
+
+  private final DriveCommand m_DriveCommand = new DriveCommand(m_Drivetrain);
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandJoystick m_driverController = new CommandJoystick(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+
+    m_Drivetrain.setDefaultCommand(m_DriveCommand);
   }
 
   /**
@@ -42,13 +46,7 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    
   }
 
   /**
@@ -56,8 +54,9 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
+
+   
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
     return Autos.exampleAuto(m_exampleSubsystem);
   }
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,14 +4,44 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
 
-  
+  /* Initializing variables */
+  CANSparkMax frontLeftMotor;
+  CANSparkMax backLeftMotor;
+  CANSparkMax frontRightMotor;
+  CANSparkMax backRightMotor;
+
+  MotorControllerGroup leftMotors;
+  MotorControllerGroup rightMotors;
+
+  DifferentialDrive differentialDrive;
 
   /** Creates a new Drivetrain. */
-  public Drivetrain() {}
+  public Drivetrain() {
+    /* Defines variables */
+    frontLeftMotor = new CANSparkMax(0, MotorType.kBrushless);
+    backLeftMotor = new CANSparkMax(1, MotorType.kBrushless);
+    frontRightMotor = new CANSparkMax(2, MotorType.kBrushless);
+    backRightMotor = new CANSparkMax(3,MotorType.kBrushless);
+
+    leftMotors = new MotorControllerGroup(frontLeftMotor, backLeftMotor);
+    rightMotors = new MotorControllerGroup(frontRightMotor,backRightMotor);
+
+    differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
+  }
+
+  public void ArcadeDrive(double forwardSpeed, double turnSpeed) {
+    differentialDrive.arcadeDrive(forwardSpeed, turnSpeed);
+  }
 
   @Override
   public void periodic() {
